584973f21eb07431b0e393d0c9f4f21d71339c24
unknown <gabemarredondo@gmail.com>
2022-09-13 19:29:57 -0700

    added questions for the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      question:
+        "A string must be inside ___________ when being assigned to variables.",
+      choices: ["quotes", "parentheses", "commas", "curly brackets"],
+      answer: "quotes",
+    },
+
+    {
+      question:
+        "A tool to see if your code is running through properly is _____________",
+      choices: ["JavaScript", "console.log", "terminal/GitBash", "for loops"],
+      answer: "console.log",
+    },
+
+    {
+      question: "What can be stored in an array?",
+      choices: ["booleans", "additional arrays", "strings", "all of the above"],
+      answer: "all of the above",
+    },
+    
+    {
+      question:
+        "An if/else statement is put inside of _____________.",
+      choices: ["quotes", "parentheses", "curly brackets", "square brackets"],
+      answer: "curly brackets",
+    },
+  
+    {
+      question: "An uncommon used data type is ___________",
+      choices: ["alerts", "booleans", "strings", "numbers"],
+      answer: "alerts",
+    },
+  
+  ];
